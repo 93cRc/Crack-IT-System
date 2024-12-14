5d7ff80951d31d9c31e6c2886b751065d8f733ec
Kamil Krakowiak <kamil_krakowiak@wp.pl>
2024-12-14 20:59:36 +0100

    push controllera

---
+++ Crack-IT System/Controller/ElectricityController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crack_IT_System.Controller {
+	internal class ElectricityController {
+
+	}
+}
